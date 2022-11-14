635f07a117a11f0e415f4e95f665c5d7156eeb56
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-11-14 14:22:45 +0900

    Add retrofit class in TVMazeApi.kt

--- core/network/src/main/java/com/project/network/retrofit/TVMazeApi.kt
+++ core/network/src/main/java/com/project/network/retrofit/TVMazeApi.kt
@@ -1,10 +1,13 @@
 package com.project.network.retrofit
 
-import com.project.network.model.NetworkCrew
-import com.project.network.model.NetworkShowResource
+import com.project.network.BuildConfig.BASE_URL
+import com.project.network.TVMazeDataSource
+import com.project.network.model.*
+import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import javax.inject.Singleton
 
 interface TVMazeApi {
     @GET("/search/shows")
@@ -16,4 +19,19 @@ interface TVMazeApi {
     fun getCrews(
         @Path("id") id: Int
     ): List<NetworkCrew>
+}
+
+@Singleton
+class TVMaze : TVMazeDataSource {
+    private val networkApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .build()
+        .create(TVMazeApi::class.java)
+
+    override suspend fun getShowResources(query: String): List<NetworkShowResource> =
+        networkApi.getShowResources(query)
+
+
+    override suspend fun getCrews(id: Int): List<NetworkCrew> =
+        networkApi.getCrews(id)
 }
