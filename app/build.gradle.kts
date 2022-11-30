df7e78e711519cb78b90bb7d3d8fee067efb7509
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-11-30 11:05:20 +0900

    Add navigation to MainActivity.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -9,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.project.show"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -48,18 +50,32 @@ android {
 
 dependencies {
 
-    val composeVersion = rootProject.extra["compose_version"]
+    implementation(project(":feature:search"))
+    implementation(project(":feature:result"))
+    implementation(project(":feature:detail"))
+
+    val composeVersion = "1.3.1"
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha01")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha02")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
+
+    val hiltVersion = "2.44.2"
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+
+    val navigationVersion = "2.5.3"
+    implementation("androidx.navigation:navigation-compose:$navigationVersion")
+
+    val systemUiController = "0.28.0"
+    implementation("com.google.accompanist:accompanist-systemuicontroller:$systemUiController")
 }
--- app/src/main/java/com/project/show/MainActivity.kt
+++ app/src/main/java/com/project/show/MainActivity.kt
@@ -7,15 +7,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.project.show.navigation.TVMazeNavHost
 import com.project.show.ui.theme.ShowTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ShowTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
+                    val navController = rememberNavController()
+                    TVMazeNavHost(navController)
                 }
             }
         }
