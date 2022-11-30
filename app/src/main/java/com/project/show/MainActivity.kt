814055c9e7d046960453253988ccf1bb501f36d9
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-11-30 11:08:37 +0900

    Update status bar color

--- app/src/main/java/com/project/show/MainActivity.kt
+++ app/src/main/java/com/project/show/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.project.show.navigation.TVMazeNavHost
 import com.project.show.ui.theme.ShowTheme
@@ -19,6 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShowTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    WindowCompat.setDecorFitsSystemWindows(window, false)
+
                     val navController = rememberNavController()
                     TVMazeNavHost(navController)
                 }
--- app/src/main/java/com/project/show/ui/theme/Theme.kt
+++ app/src/main/java/com/project/show/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.project.show.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -10,19 +9,19 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Orange,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Orange,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
@@ -53,11 +52,18 @@ fun ShowTheme(
         else -> LightColorScheme
     }
     val view = LocalView.current
+
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = !darkTheme
+
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            systemUiController.setSystemBarsColor(
+                color = Color.Transparent,
+                darkIcons = useDarkIcons
+            )
         }
+
     }
 
     MaterialTheme(
