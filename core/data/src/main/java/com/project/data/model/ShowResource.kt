98fcb1bff61363f04a2fb338a15b9a6a01e0f301
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-12-05 15:52:46 +0900

    Remove unused parameter

--- core/data/src/main/java/com/project/data/model/ShowResource.kt
+++ core/data/src/main/java/com/project/data/model/ShowResource.kt
@@ -4,20 +4,15 @@ import com.project.model.ShowResource
 import com.project.network.model.NetworkShowResource
 
 fun NetworkShowResource.toShowResource() = ShowResource(
-    id = show.id,
-    name = show.name,
-    type = show.type,
-    language = show.language,
-    genres = show.genres,
-    status = show.status,
-    averageRuntime = show.averageRuntime,
-    runtime = show.runtime,
-    premiered = show.premiered,
-    ended = show.ended,
-    officialSite = show.officialSite,
-    schedule = show.schedule,
-    rating = show.rating?.average,
-    network = show.network,
-    image = show.image,
-    summary = show.summary
+    officialSite = officialSite,
+    langueage = language,
+    genres = genres,
+    schedule = schedule,
+    status = status,
+    averageRuntime = averageRuntime,
+    premiered = premiered,
+    ended = ended,
+    image = image?.original,
+    summary = summary,
+    name = name
 )
--- core/model/src/main/java/com/project/model/ShowResource.kt
+++ core/model/src/main/java/com/project/model/ShowResource.kt
@@ -1,38 +1,20 @@
 package com.project.model
 
 data class ShowResource(
-    val id: Int,
+    val officialSite: String?,
     val name: String,
-    val type: String,
-    val language: String?,
+    val langueage: String?,
     val genres: List<String>?,
+    val schedule: Schedule,
     val status: String,
     val averageRuntime: Int?,
-    val runtime: Int?,
     val premiered: String?,
     val ended: String?,
-    val officialSite: String?,
-    val schedule: Schedule?,
-    val rating: Double?,
-    val network: Network?,
-    val image: Image?,
-    val summary: String?,
+    val image: String?,
+    val summary: String?
 )
 
 data class Schedule(
     val days: List<String>,
-    val time: String
+    val time: String?
 )
-
-data class Network(
-    val country: Country,
-    val id: Int,
-    val name: String,
-    val officialSite: String
-) {
-    data class Country(
-        val code: String,
-        val name: String,
-        val timezone: String
-    )
-}
